23875780fe909ac71192cfc754c4028e92af4b94
harsh17-bit <hr9012454@gmail.com>
2026-03-02 10:51:15 +0530

    Updateo for making picture size optimization

--- server/server.js
+++ server/server.js
@@ -15,9 +15,15 @@ app.use(
     contentSecurityPolicy: {
       directives: {
         defaultSrc: ["'self'"],
+
         scriptSrc: ["'self'"],
-        styleSrc: ["'self'", "'unsafe-inline'"],
+
+        styleSrc: ["'self'", "'unsafe-inline'", 'https://fonts.googleapis.com'],
+
+        fontSrc: ["'self'", 'https://fonts.gstatic.com', 'data:'],
+
         imgSrc: ["'self'", 'data:', 'https:'],
+
         connectSrc: ["'self'", 'https://api.openweathermap.org'],
       },
     },
